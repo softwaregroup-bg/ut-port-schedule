c0b9f5be0dc0c6ab7bd992038def2be8232fcab5
elin.ahmedov <elin.ahmedov@softwaregroup-bg.com>
2015-10-16 17:58:33 +0300

    fix(runAfterStart): should job must be run after port starts, eg if some jobs start is skipped because of port not working

--- index.js
+++ index.js
@@ -4,6 +4,75 @@ var cron = require('cron');
 var through2 = require('through2');
 var push;
 var jobs = {};
+function CheckForImmediateRun(job) {
+    if (!job.lastRun || !(job.lastRun instanceof Date)) {
+        return false;
+    }
+    var lastRunMin = job.lastRun.getMinutes();
+    var lastRunHour = job.lastRun.getHours();
+    var lastRunDay = job.lastRun.getDate();
+    var lastRunMonth = (job.lastRun.getMonth()).toString();
+    var nlastRunYear = job.lastRun.getFullYear();
+    var lastRunYear = nlastRunYear.toString();
+    var lastRunYearMonth = parseInt(lastRunYear + (lastRunMonth.length == 1 ? ('0' + lastRunMonth) : lastRunMonth));
+    var cronTime = job.cronTime;
+    var cMinute = cronTime.minute;
+    var cHour = cronTime.hour;
+    var cDay = cronTime.dayOfMonth;
+    var cMonth = cronTime.month;
+    var cWeekDays = cronTime.dayOfWeek;
+
+    var eMonth = null;
+    for (m in cMonth) {
+        var mStr = m.toString();
+        var cYearMonth = parseInt(lastRunYear + (mStr.length == 1 ? ('0' + mStr) : mStr));
+        if (cYearMonth >= lastRunYearMonth) {
+            eMonth = m;
+            break;
+        }
+        eMonth = m;
+    }
+    var eDay = null;
+    for (d in cDay) {
+        if (d >= lastRunDay) {
+            eDay = d;
+            break;
+        }
+        eDay = d;
+    }
+    var eHour = null;
+    for (h in cHour) {
+        if (h >= lastRunHour) {
+            eHour = h;
+            break;
+        }
+        eHour = h;
+    }
+    var eMinute = null;
+    for (n in cMinute) {
+        if (n >= lastRunMin) {
+            eMinute = n;
+            break;
+        }
+        eMinute = n;
+    }
+
+    var nextDateTime = new Date(nlastRunYear, eMonth, eDay, eHour, eMinute, 0, 0);
+
+    for (var w = 0; w <= 6; w++) {
+        var nextWDay = nextDateTime.getDay();
+        if (nextWDay in cWeekDays) {
+            break;
+        }
+        nextDateTime.setDate(nextDateTime.getDate() + 1);
+    }
+
+    var currD = new Date();
+    var nextTime = Math.floor(nextDateTime.getTime() / 1000);
+    var currTime = Math.floor(currD.getTime() / 1000);
+
+    return (currTime > nextTime);
+}
 
 function UtCron() {
     Port.call(this);
@@ -44,23 +113,35 @@ UtCron.prototype.addJobs = function(jobs) {
 }
 
 UtCron.prototype.addJob = function(name, job) {
+    if (!jobs[name]) {
+        this.log.info && this.log.info({opcode:'Schedule',msg:`Add Job ${name}`});
+        jobs[name] = new cron.CronJob({
+            cronTime: job.pattern,
+            onTick: function() {
+                job.lastRun = jobs[name].lastRun;
+                jobs[name].lastRun = new Date();
+                push.write({$$:{opcode: name, mtid: 'notification'}, messageId: name, payload: job})
+            },
+            start: true,
+            timeZone: undefined,
+            context: undefined
+        });
+        if (CheckForImmediateRun(job)) {
+            push.write({$$:{opcode: name, mtid: 'notification'}, messageId: name, payload: job})
+        }
+    } else {
+        this.log.info && this.log.info({opcode:'Schedule',msg:`Cannot Add Job ${name}, allready exists, use updateJob`});
+    }
+}
+
+UtCron.prototype.updateJob = function(name, job) {
     if (jobs[name]) {
         this.log.info && this.log.info({opcode:'Schedule',msg:`Remove Job ${name}`});
         jobs[name].stop();
+        job.lastRun = jobs[name].lastRun;
         delete jobs[name];
     }
-    this.log.info && this.log.info({opcode:'Schedule',msg:`Add Job ${name}`});
-    jobs[name] = new cron.CronJob({
-        cronTime: job.pattern,
-        onTick: function() {
-            job.lastRun = jobs[name].lastRun;
-            jobs[name].lastRun = new Date();
-            push.write({$$:{opcode: name, mtid: 'notification'}, messageId: name, payload: job})
-        },
-        start: true,
-        timeZone: undefined,
-        context: undefined
-    });
+    this.addJob(name, job);
 }
 
 module.exports = UtCron;
